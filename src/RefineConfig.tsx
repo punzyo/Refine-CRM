9efe699caaa3d36f4654c14528cfaa99e6069f70
punzyo <hellomickey004@gmail.com>
2025-04-03 18:03:13 +0800

    feat: 更新 AdminList 組件以使用 useList 並顯示管理員列表

--- src/RefineConfig.tsx
+++ src/RefineConfig.tsx
@@ -4,13 +4,14 @@ import routerBindings from '@refinedev/react-router'
 import dataProvider from '@refinedev/simple-rest'
 import { authProvider } from './authProvider'
 import resources from './resource'
+import axiosInstance from './axiosInstance'
 
 const RefineConfig: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const API_URL = 'http://localhost:3001'
 
   return (
     <Refine
-      dataProvider={dataProvider(API_URL)}
+      dataProvider={dataProvider(API_URL,axiosInstance)}
       notificationProvider={useNotificationProvider}
       authProvider={authProvider}
       routerProvider={routerBindings}
--- src/pages/admin/AdminList.tsx
+++ src/pages/admin/AdminList.tsx
@@ -1,9 +1,32 @@
-import { List } from "@refinedev/mui";
-import { usePermissions } from "@refinedev/core";
-export default function AdminList() {
-    console.log('admin');
-    const { data, isLoading } = usePermissions();
-    console.log(data, isLoading);
-    
-    return <List>管理員列表</List>;
+import { useList } from '@refinedev/core'
+import { List } from '@refinedev/mui'
+import { Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material'
+
+const AdminList = () => {
+  const { data, isLoading } = useList({ resource: 'admin' })
+
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <List>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Email</TableCell>
+            <TableCell>建立時間</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.data.map((admin) => (
+            <TableRow key={admin.id}>
+              <TableCell>{admin.email}</TableCell>
+              <TableCell>{new Date(admin.createdAt).toLocaleString()}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </List>
+  )
 }
+
+export default AdminList
